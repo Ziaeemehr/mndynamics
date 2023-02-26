fff601e803399d29d75a0076f88616444f004afc
Ziaeemehr <a.ziaeemehr@gmail.com>
2023-02-26 19:11:54 +0100

    test added.

--- mndynamics/tests/test_HH.py
+++ mndynamics/tests/test_HH.py
@@ -1,5 +1,6 @@
 import unittest
 import numpy as np
+from mndynamics.models.py.ch04 import HH_SOLUTION
 from mndynamics.models.py.HH_Base import HH, HH_Reduced
 from mndynamics.models.py.ch03 import HH_GATING_VARIABLES
 
@@ -49,7 +50,25 @@ class TestModules(unittest.TestCase):
 
         self.assertEqual(np.abs(mean+63.107)<0.1, True)
         self.assertEqual(np.abs(std-27.35)<0.1, True)
+    
+    def test_HH_Solution(self):
+
+        obj = HH_SOLUTION()
+
+        obj = HH_SOLUTION({"i_ext": 10.0, "v0":-50.0})
+        data = obj.simulate(tspan=np.arange(0, 50, 0.01))
+        t = data["t"]
+        v = data["v"]
+
+        mean = np.mean(v)
+        std = np.std(v)
+        # print(mean, std)
+        self.assertEqual(np.abs(mean+62.21)<0.1, True)
+        self.assertEqual(np.abs(std-22.47)<0.1, True)
+
 
 
 if __name__ == '__main__':
     unittest.main()
+    # obj = TestModules()
+    # obj.test_HH_Solution()
